703700a6dad6e8fa548dd14e32847810ea74a1d5
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2022-04-26 14:34:22 +0300

    update

---
+++ Tek Yonlu Dairesel Liste Musteri Kayit Uygulamasi/Tek Yonlu Dairesel Liste Musteri Kayit Uygulamasi.cpp
@@ -0,0 +1,188 @@
+﻿#include<iostream>
+using namespace std;
+
+struct node {
+	int id;
+	string ad;
+	string soyad;
+	string adres;
+	string urudnAd;
+	int ucret;
+	node* next;
+};
+node* head = NULL;
+
+void ekle(int id, string ad, string soyad, string adres, string urudnAd, int ucret)
+{
+	system("cls");
+	node* eleman = new node;
+	eleman->id = id;
+	eleman->ad = ad;
+	eleman->soyad = soyad;
+	eleman->adres = adres;
+	eleman->urudnAd = urudnAd;
+	eleman->ucret = ucret;
+
+	if (head == NULL) {
+		head = eleman;
+		head->next = head;
+		cout << id << " numarali musteri listeye ilk kayit olarak eklendi" << endl;
+	}
+	else if (head->next == head) {
+		eleman->next = head;
+		head = eleman;
+		head->next->next = head;
+		cout << id << " numarali musteri listeye eklendi" << endl;
+	}
+	else {
+		node* temp = head;
+		while (temp->next != head)
+		{
+			temp = temp->next;
+		}
+		eleman->next = head;
+		head = eleman;
+		temp->next = head;
+		cout << id << " numarali musteri listeye eklendi" << endl;
+	}
+}
+void cikar(int id) {
+	bool sonuc = false;
+	system("cls");
+	if (head == NULL) {
+		cout << "Liste bos cikartilacak musteri yoktur" << endl;
+		sonuc = true;
+	}
+	else if (head->next == head && head->id == id)
+	{
+		delete head;
+		head = NULL;
+		cout << id << " numarali son musteri listeden silindi" << endl;
+		sonuc = true;
+	}
+	else {
+		node* temp = head;
+		while (temp->next != head)
+		{
+			if (temp->next->id == id) {
+				node* temp2 = temp;
+				node* temp3 = temp->next->next;
+				delete temp->next;
+				temp2->next = temp3;
+				cout << id << " numarali musteri silindi" << endl;
+				sonuc = true;
+				break;
+			}
+			temp = temp->next;
+		}
+	}
+	if (!sonuc)
+		cout << id << " numarali musteri liste yoktur" << endl;
+}
+void ara(int id) {
+	bool sonuc = false;
+	system("cls");
+	if (head == NULL) {
+		cout << " liste bostur, her hangi bir musteri kaydi yoktur " << endl;
+		bool sonuc = true;
+	}
+	else {
+		node* temp = head;
+		while (temp->next != head)
+		{
+			if (temp->id == id) {
+				cout << id << " numarali musteri bilgileri" << endl;
+				cout << "ad      :" << head->ad << endl;
+				cout << "soyad   :" << head->soyad << endl;
+				cout << "adres   :" << head->adres << endl;
+				cout << "UrunAd  :" << head->urudnAd << endl;
+				cout << "Ucret   :" << head->ucret << endl;
+				break;
+			}
+			temp = temp->next;
+		}
+		if (!sonuc)
+			cout << id << " numarali musteri liste yoktur" << endl;
+	}
+}
+void yazdir()
+{
+	system("cls");
+	if (head == NULL)
+	{
+		cout << " liste bostur, her hangi bir musteri kaydi yoktur " << endl;
+	}
+	else
+	{
+		node* temp = head;
+		while (temp->next != head)
+		{
+			cout << temp->id << " numarali musteri bilgileri... \n";
+			cout << "ad      :" << temp->ad << endl;
+			cout << "soyad   :" << temp->soyad << endl;
+			cout << "adres   :" << temp->adres << endl;
+			cout << "UrunAd  :" << temp->urudnAd << endl;
+			cout << "Ucret   :" << temp->ucret << endl;
+			temp = temp->next;
+
+		}
+		cout << temp->id << " numarali musteri bilgileri...\n";
+		cout << "ad      :" << temp->ad << endl;
+		cout << "soyad   :" << temp->soyad << endl;
+		cout << "adres   :" << temp->adres << endl;
+		cout << "UrunAd  :" << temp->urudnAd << endl;
+		cout << "Ucret   :" << temp->ucret << endl;
+	}
+
+}
+
+int main() {
+	int id, ucret, secim;
+	string ad, soyad, adres, urunAd;
+	while (1)
+	{
+		cout << endl;
+		cout << "TYDABL Musteri Kayit Uygulamasi" << endl;
+		cout << "1-- yeni kayit" << endl;
+		cout << "2-- kayit sil" << endl;
+		cout << "3-- kayit ara" << endl;
+		cout << "4-- tum kayitlari getir" << endl;
+		cout << "0-- CIKIS" << endl;
+		cin >> secim;
+		switch (secim)
+		{
+		case 1:
+			cout << "Yeni kayit yapilacak musteri bilgileri : " << endl;
+			cout << "id      :"; cin >> id;
+			cout << "Ad      :"; cin >> ad;
+			cout << "Soyad   :"; cin >> soyad;
+			cout << "Adres   :"; cin >> adres;
+			cout << "UrunAd  :"; cin >> urunAd;
+			cout << "Ucret   :"; cin >> ucret;
+			ekle(id, ad, soyad, adres, urunAd, ucret);
+			break;
+		case 2:
+			cout << " Silinecek musteri id: "; cin >> id;
+			cikar(id);
+			break;
+		case 3:
+			cout << " Aranan musteri id:"; cin >> id;
+			ara(id);
+			break;
+		case 4:
+			yazdir();
+			break;
+		case 0:
+			cout << "Programdan cikiliyor..." << endl;
+			system("pause");
+			return 0;
+			break;
+		default:
+			cout << "Lutfen listede belirtilen secenek numaralarini giriniz" << endl;
+			break;
+		}
+
+	}
+
+
+}
---
+++ Tek yonlu dairesel liste notlari/Tek yonlu dairesel liste notlari.cpp
@@ -0,0 +1,258 @@
+﻿#include<iostream>
+using namespace std;
+
+struct node {
+	int veri;
+	node* next;
+};
+node* head = NULL;		//bas düğüm
+void basaekle(int key) {
+	node* eleman = new node;	//eleman oluştu
+	eleman->veri = key;
+	if (head == NULL)            //ilk elelman boşsa
+	{
+		head = eleman;           //eleman hem baş hem son elelman olcak
+		head->next = head;
+		cout << head->veri << "ilk kayit eklendi" << endl;
+	}
+	else {
+		if (head->next == head) //tekdüğüm varsa
+		{
+			eleman->next = head;
+			head->next = eleman;
+			head = eleman;
+			cout << head->veri << "kayit basa eklendi" << endl;
+		}
+		else    //listede birden fazla düğüm varsa
+		{
+			node* temp = head;
+			while (temp->next != head)
+			{
+				temp = temp->next;
+			}
+			eleman->next = head;
+			head = eleman;
+			temp->next = head;
+			cout << head->veri << "kayit basa eklendi" << endl;
+		}
+	}
+}
+void sonaekle(int key) {
+	node* eleman = new node;	//eleman oluştu
+	eleman->veri = key;
+	if (head == NULL)            //ilk elelman boşsa
+	{
+		head = eleman;           //eleman hem baş hem son elelman olcak
+		head->next = head;
+		cout << head->veri << "ilk kayit eklendi" << endl;
+	}
+	else {
+		if (head->next == head) //tekdüğüm varsa
+		{
+
+			head->next = eleman;
+			eleman->next = head;
+			cout << head->veri << "kayit sona eklendi" << endl;
+		}
+		else    //listede birden fazla düğm varsa
+		{
+			node* temp = head;
+			while (temp->next != head)
+			{
+				temp = temp->next;
+			}
+			temp->next = eleman;
+			eleman->next = head;
+			cout << eleman->veri << "kayit sona eklendi" << endl;
+		}
+	}
+}
+void yazdir()
+{
+	system("cls");
+
+	node* temp = head;
+	if (head == NULL)
+		cout << "liste bos" << endl;
+	else {
+		while (temp->next != head) {
+			cout << temp->veri << " ";
+			temp = temp->next;
+		}
+		cout << temp->veri << " ";
+	}
+}
+void arayaekle(int key, int sayi) {
+	bool sonuc = false;
+	node* eleman = new node;	//eleman oluştu
+	eleman->veri = key;
+	if (head == NULL)            //ilk elelman boşsa
+	{
+		head = eleman;           //eleman hem baş hem son elelman olcak
+		head->next = head;
+		cout << head->veri << " Listeye ilk kayit eklendi" << endl;
+	}
+	else {
+		if (head->next == head) //tekdüğüm varsa
+		{
+			eleman->next = head;
+			head->next = eleman;
+			head = eleman;
+			cout << head->veri << " kayit basa eklendi" << endl;
+			sonuc = true;
+		}
+		else
+		{
+			node* temp = head;
+			while (temp->next != head)
+			{
+				if (temp->next->veri == sayi) {
+					node* temp2 = temp;
+					node* temp3 = temp->next;
+					temp2->next = eleman;
+					eleman->next = temp3;
+					cout << " araya elelman eklendi" << endl;
+					sonuc = true;
+					break;
+				}
+				temp = temp->next;
+			}
+		}
+		if (!sonuc) {
+			cout << "ekleme yapilmadi" << endl;
+		}
+	}
+}
+void bastanSil() {
+	if (head == NULL) {
+		cout << "liste bos,silinecek eleman yok" << endl;
+	}
+	else {
+		if (head->next == head) //tekdüğüm varsa
+		{
+			delete head;
+			head = NULL;
+			cout << "Listede kalan son dugum silindi" << endl;
+		}
+		else {
+			node* temp = head;
+			while (temp->next != head)
+			{
+				temp = temp->next;
+			}
+			node* temp2 = head->next;
+			delete head;
+			head = temp2;
+			temp->next = head;
+			cout << "bastaki eleman silindi" << endl;
+		}
+	}
+}
+void sondanSil() {
+	if (head == NULL) {
+		cout << "liste bos,silinecek eleman yok" << endl;
+	}
+	else {
+		if (head->next == head) //tekdüğüm varsa
+		{
+			delete head;
+			head = NULL;
+			cout << "Listede kalan son dugum silindi" << endl;
+		}
+		else {
+			node* temp = head;
+			while (temp->next->next != head)
+			{
+				temp = temp->next;
+			}
+			delete temp->next;
+			temp->next = head;
+			cout << "sondaki dugum silindi" << endl;
+		}
+	}
+}
+void aradanSil(int key) {
+	bool sonuc = false;
+	if (head == NULL) {
+		cout << "liste bos,silinecek eleman yok" << endl;
+	}
+	else {
+		if (head->next == head && head->veri == key) //tekdüğüm varsa
+		{
+			delete head;
+			head = NULL;
+			cout << "Listede kalan son dugum silindi" << endl;
+		}
+		else {
+			node* temp = head;
+			while (temp->next != head)
+			{
+				if (temp->next->veri == key)
+				{
+					node* temp2 = temp;
+					node* temp3 = temp->next->next;
+					delete temp->next;
+					temp2->next = temp3;
+					cout << "aradaki dugum silindi" << endl;
+					sonuc = true;
+					break;
+				}
+				temp = temp->next;
+			}
+		}
+		if (!sonuc)
+			cout << key << " degerli dugum listede yoktur" << endl;
+	}
+}
+int main() {
+	int sayi, sayi2, secim;
+	while (1)
+	{
+		cout << endl;
+		cout << "TYDBL uygulamasi" << endl;
+		cout << "1--Basa ekle" << endl;
+		cout << "2--Sona ekle" << endl;
+		cout << "3--Araya ekle" << endl;
+		cout << "4--Bastan sil" << endl;
+		cout << "5--Sondan sil" << endl;
+		cout << "6--Aradan sil" << endl;
+		cout << "7--Yazdir" << endl;
+		cin >> secim;
+		switch (secim)
+		{
+		case 1:cout << "basa eklenecek sayi: ";
+			cin >> sayi;
+			basaekle(sayi);
+			break;
+		case 2:cout << "sona eklenecek sayi: ";
+			cin >> sayi;
+			sonaekle(sayi);
+			break;
+		case 3:
+			cout << "Araya eklenecek sayi: " << endl;
+			cin >> sayi;
+			cout << "hangi sayidan once eklensin" << endl;
+			cin >> sayi2;
+			arayaekle(sayi, sayi2);
+			break;
+		case 4:
+			bastanSil();
+			break;
+		case 5:
+			sondanSil();
+			break;
+		case 6:cout << "aradan silmek istediginiz deger:";
+			cin >> sayi;
+			aradanSil(sayi);
+			break;
+		case 7:
+			yazdir();
+			break;
+		default:
+			cout << "Lutfen seceneklerdeki numaralardaki sayilari giriniz!!!" << endl;
+			break;
+		}
+
+	}
+
+}
